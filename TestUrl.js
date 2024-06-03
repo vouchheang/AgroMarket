d663344dc3b025eae113ce6a809a887c4622db61
vouchheang <vouchheang.mom@institute.pse.ngo>
2024-06-03 10:59:54 +0700

    mergecode

--- TestUrl.js
+++
@@ -1,8 +0,0 @@
-console.log("Window Location:", window.location);
-
-const myFruitsValues = window.location.search;
-console.log("Fruits && Values:", myFruitsValues) ;
-
-const urlParam = new URLSearchParams(myFruitsValues);
-
-URLSearchParams.get()
